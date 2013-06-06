fa56a8eb54cbbd0306c143e9535b6ac60e43563d
Raul Portales <raul@plattysoft.com>
2013-06-06 17:21:51 +0200

    fixing indentation

--- src/com/plattysoft/ui/GridViewWithHeaderBaseAdapter.java
+++ src/com/plattysoft/ui/GridViewWithHeaderBaseAdapter.java
@@ -14,10 +14,10 @@ import android.widget.LinearLayout;
  */
 public abstract class GridViewWithHeaderBaseAdapter extends BaseAdapter {
 
-    public interface GridItemClickListener {
-    	
+	public interface GridItemClickListener {
+
 		void onGridItemClicked(View v, int position, long itemId);
-		
+
 	}
 
 	private class ListItemClickListener implements OnClickListener {
@@ -39,104 +39,104 @@ public abstract class GridViewWithHeaderBaseAdapter extends BaseAdapter {
 	private Context mContext;
 	private GridItemClickListener mGridItemClickListener;
 
-    public GridViewWithHeaderBaseAdapter(Context context) {
-    	mContext = context;
-        mNumColumns = 1;
-    }
-    
+	public GridViewWithHeaderBaseAdapter(Context context) {
+		mContext = context;
+		mNumColumns = 1;
+	}
+
 	public final void setOnGridClickListener(GridItemClickListener listener) {
 		mGridItemClickListener = listener;
 	}
-    
-    private final void onGridItemClicked(View v, int position) {
-    	if (mGridItemClickListener != null) {
-    		mGridItemClickListener.onGridItemClicked(v, position, getItemId(position));
-    	}
+
+	private final void onGridItemClicked(View v, int position) {
+		if (mGridItemClickListener != null) {
+			mGridItemClickListener.onGridItemClicked(v, position, getItemId(position));
+		}
 	}
 
 	@Override
-    public final int getCount() {
-        return (int) Math.ceil(getItemCount()*1f / getNumColumns());
-    }
+	public final int getCount() {
+		return (int) Math.ceil(getItemCount()*1f / getNumColumns());
+	}
 
-    public abstract int getItemCount();
+	public abstract int getItemCount();
 
-    protected abstract View getItemView(int position, View view, ViewGroup parent);
+	protected abstract View getItemView(int position, View view, ViewGroup parent);
 
-    @Override
-    public final View getView(int position, View view, ViewGroup viewGroup) {
-    	LinearLayout layout;
-    	int columnWidth = 0;
-    	if (viewGroup !=  null) {
-    		columnWidth = viewGroup.getWidth()/mNumColumns;    		
-    	}
-        // Make it be rows of the number of columns
-        if (view == null) {
-            // This is items view
-        	layout = new LinearLayout(mContext);
-        	layout.setOrientation(LinearLayout.HORIZONTAL);
-        	// Here the view is not measured, get the width of the parent if not null
-            // Now add the sub views to it
-            for (int i = 0; i < mNumColumns; i++) {
-            	int currentPos = position * mNumColumns + i;
-            	// Get the new View
-            	View insideView;
-            	if (currentPos < getItemCount()) {            		
-            		insideView = getItemView(currentPos, null, viewGroup);            	
-            	}
-            	else {
-            		insideView = new View(mContext);
-        			layout.addView(insideView);
-            	}            	
-            	layout.addView(insideView);
-            	// Set the width of this column
-        		LayoutParams params = insideView.getLayoutParams();
-        		params.width = columnWidth;
-        		insideView.setLayoutParams(params);
-            }            
-        }
-        else {
-        	layout = (LinearLayout) view;
-        	if (columnWidth == 0) {
-        		columnWidth = layout.getWidth()/mNumColumns;
-        	}
-        	for (int i = 0; i < mNumColumns; i++) {
-        		int currentPos = position * mNumColumns + i;
-        		View insideView = layout.getChildAt(i);
-        		// If there are less views than objects. add a view here
-        		if (insideView == null) {
-        			insideView = new View(mContext);
-        			layout.addView(insideView);
-        		}
-        		// Set the width of this column
-        		LayoutParams params = insideView.getLayoutParams();
-        		params.width = columnWidth;
-        		insideView.setLayoutParams(params);
-        		
-        		if (currentPos < getItemCount()) {
-        			insideView.setVisibility(View.VISIBLE);
-        			// Populate the view
-        			View theView = getItemView(currentPos, insideView, viewGroup);
-        			theView.setOnClickListener(new ListItemClickListener (currentPos));
-        			if (!theView.equals(insideView)) {
-        				// DO NOT CHANGE THE VIEWS
-        			}
-        		}
-        		else {
-        			insideView.setVisibility(View.INVISIBLE);
-        		}
-        	}
-        }
-        
-        return layout;
-    }
+	@Override
+	public final View getView(int position, View view, ViewGroup viewGroup) {
+		LinearLayout layout;
+		int columnWidth = 0;
+		if (viewGroup !=  null) {
+			columnWidth = viewGroup.getWidth()/mNumColumns;    		
+		}
+		// Make it be rows of the number of columns
+		if (view == null) {
+			// This is items view
+			layout = new LinearLayout(mContext);
+			layout.setOrientation(LinearLayout.HORIZONTAL);
+			// Here the view is not measured, get the width of the parent if not null
+			// Now add the sub views to it
+			for (int i = 0; i < mNumColumns; i++) {
+				int currentPos = position * mNumColumns + i;
+				// Get the new View
+				View insideView;
+				if (currentPos < getItemCount()) {            		
+					insideView = getItemView(currentPos, null, viewGroup);            	
+				}
+				else {
+					insideView = new View(mContext);
+					layout.addView(insideView);
+				}            	
+				layout.addView(insideView);
+				// Set the width of this column
+				LayoutParams params = insideView.getLayoutParams();
+				params.width = columnWidth;
+				insideView.setLayoutParams(params);
+			}            
+		}
+		else {
+			layout = (LinearLayout) view;
+			if (columnWidth == 0) {
+				columnWidth = layout.getWidth()/mNumColumns;
+			}
+			for (int i = 0; i < mNumColumns; i++) {
+				int currentPos = position * mNumColumns + i;
+				View insideView = layout.getChildAt(i);
+				// If there are less views than objects. add a view here
+				if (insideView == null) {
+					insideView = new View(mContext);
+					layout.addView(insideView);
+				}
+				// Set the width of this column
+				LayoutParams params = insideView.getLayoutParams();
+				params.width = columnWidth;
+				insideView.setLayoutParams(params);
 
-    public final int getNumColumns() {
-        return mNumColumns;
-    }
+				if (currentPos < getItemCount()) {
+					insideView.setVisibility(View.VISIBLE);
+					// Populate the view
+					View theView = getItemView(currentPos, insideView, viewGroup);
+					theView.setOnClickListener(new ListItemClickListener (currentPos));
+					if (!theView.equals(insideView)) {
+						// DO NOT CHANGE THE VIEWS
+					}
+				}
+				else {
+					insideView.setVisibility(View.INVISIBLE);
+				}
+			}
+		}
 
-    public final void setNumColumns(int numColumns) {
-        mNumColumns = numColumns;
-        notifyDataSetChanged();
-    }
+		return layout;
+	}
+
+	public final int getNumColumns() {
+		return mNumColumns;
+	}
+
+	public final void setNumColumns(int numColumns) {
+		mNumColumns = numColumns;
+		notifyDataSetChanged();
+	}
 }
